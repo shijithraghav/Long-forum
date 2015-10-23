09784379a09e863c3d7d776df13e7e5f2f1c1798
shijithraghav <srk6kichu@gmail.com>
2015-10-23 12:11:48 +0530

    user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if current_user.id == @user.id
-      @invites = Invite.where(:user_id => @user.id, :status => 'false')
+      @invites = Invite.where(:user_id => @user.id, :status => 'false').paginate(:per_page => 1, :page => params[:page])
     end
   end
 end
